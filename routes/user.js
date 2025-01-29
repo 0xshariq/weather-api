6d204f1eebb618b70b35663f04a8d289c87c5c97
Shariq Choudhary <khanshariq92213@gmail.com>
2025-01-29 22:26:56 +0530

    Minor Change

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,9 @@
-import express from "express"
-import { createUser, loginUser, logoutUser } from "../controllers/user.js"
+import express from "express";
+import { createUser, loginUser, logoutUser } from "../controllers/user.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", createUser)
-router.post("/login", loginUser)
-router.get("/logout", logoutUser)
-export default router
-
+router.post("/register", createUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+export default router;
